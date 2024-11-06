29098564846d69c6f5115e9552a12b83f8a57f38
«jordi» <jordivega@iesebre.com>
2024-11-06 19:27:36 +0100

    afegit bookcontroller, model i vista index

--- routes.php
+++ routes.php
@@ -5,4 +5,13 @@ return [
     '/home' => '../App/Controllers/HomeController.php@index',
     '/index' => '../App/Controllers/HomeController.php@index',
     '/index.php' => '../App/Controllers/HomeController.php@index',
+
+    '/books' => '../App/Controllers/BookController.php@index',
+    '/books/create' => '../App/Controllers/BookController.php@create',
+    '/books/store' => '../App/Controllers/BookController.php@store',
+    '/books/edit/{id}' => '../App/Controllers/BookController.php@edit',
+    '/books/update/{id}' => '../App/Controllers/BookController.php@update',
+    '/books/delete/{id}' => '../App/Controllers/BookController.php@delete',
+    '/books/destroy/{id}' => '../App/Controllers/BookController.php@destroy',
+    '/books/confirm-delete/{id}' => '../App/Controllers/BookController.php@confirmDelete',
 ];
